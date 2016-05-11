f5432bc3a7330b3f726b9772b097b168bf4b79c8
aircord <aircord_staff@ac-MBP15.local>
2016-05-11 16:38:54 +0900

    add fonts, create UI

--- acFonts/Assets/script/ChangeAllChildValue.cs
+++ acFonts/Assets/script/ChangeAllChildValue.cs
@@ -1,12 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class ChangeAllChildValue : MonoBehaviour {
 
-	public float value1;
-	public float value2;
-	public float value3;
+	private float value1;
+	private float value2;
+	private float value3;
 	public Color color;
+	private GameObject Value1SliderGet;
+	private GameObject Value2SliderGet;
+	private GameObject Value3SliderGet;
 
 	// Use this for initialization
 	void Start () {
@@ -17,6 +21,15 @@ public class ChangeAllChildValue : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		Renderer[] renderers = GetComponentsInChildren<Renderer>();
+		Value1SliderGet = GameObject.Find ("Value 1 Slider");
+		value1 = Value1SliderGet.GetComponent<Slider> ().value;
+
+		Value2SliderGet = GameObject.Find ("Value 2 Slider");
+		value2 = Value2SliderGet.GetComponent<Slider> ().value;
+
+		Value3SliderGet = GameObject.Find ("Value 3 Slider");
+		value3 = Value3SliderGet.GetComponent<Slider> ().value;
+	
 		for (int i = 0; i < renderers.Length; i++) {
 			renderers [i].material.SetFloat ("_Value1", value1);
 			renderers [i].material.SetFloat ("_Value2", value2);
